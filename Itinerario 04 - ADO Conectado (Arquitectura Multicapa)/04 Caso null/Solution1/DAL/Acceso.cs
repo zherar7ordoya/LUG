34c3485747dd13e7dae8470bfbb009238211ac97
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-01-11 12:02:04 -0300

    2024-01-11

---
+++ Itinerario 04 - ADO Conectado (Arquitectura Multicapa)/04 Caso null/Solution1/DAL/Acceso.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DAL
+{
+    public class Acceso
+    {
+        private readonly SqlConnection connection = new SqlConnection(
+            @"Data Source=(LocalDB)\MSSQLLocalDB;
+            Initial Catalog=MiBasev2;
+            Integrated Security=True");
+
+        public DataSet Leer(string pSQL)
+        {
+            DataSet dataset = new DataSet();
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(pSQL, connection);
+                adapter.Fill(dataset);
+            }
+            catch (SqlException ex) { throw ex; }
+            catch (Exception ex) { throw ex; }
+            finally { connection.Close(); }
+
+            return dataset;
+        }
+
+        public bool Escribir(string pSQL)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand
+            {
+                CommandType = CommandType.Text,
+                Connection = connection,
+                CommandText = pSQL
+            };
+            try
+            {
+                int respuesta = command.ExecuteNonQuery();
+                return true;
+            }
+            catch (SqlException ex) { throw ex; }
+            finally { connection.Close(); }
+        }
+
+    }
+}
