f22737748389a37c912a2588407530e2736eb7e0
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-02-01 12:56:43 -0300

    2024-02-01

--- (FINAL)/(Prinzo) Ejercicio SQL y XML/DOO/Renta.cs
+++ (FINAL)/(Prinzo) Ejercicio SQL y XML/DOO/Renta.cs
@@ -3,15 +3,15 @@ public class Renta
     // Atributos
     public Cliente Cliente { get; set; }
     public Vehiculo Vehiculo { get; set; }
-    public int CantidadDias { get; set; }
+    public int DiasRentados { get; set; }
     public decimal Importe { get; set; }
 
     // Constructor
-    public Renta(Cliente cliente, Vehiculo vehiculo, int cantidadDias)
+    public Renta(Cliente cliente, Vehiculo vehiculo, int diasRentados)
     {
         Cliente = cliente;
         Vehiculo = vehiculo;
-        CantidadDias = cantidadDias;
+        DiasRentados = diasRentados;
         CalcularImporte();
     }
 
@@ -21,7 +21,7 @@ public class Renta
         // Lógica para calcular el importe basado en la cantidad de días, tipo de vehículo, etc.
         // Puedes ajustar esta lógica según tus requerimientos específicos.
         // Aquí se asume que existe un método en la clase Vehiculo para obtener el costo diario.
-        Importe = CantidadDias * Vehiculo.ObtenerCostoDiario();
+        Importe = DiasRentados * Vehiculo.ObtenerCostoDiario();
     }
 
     // Otros métodos, si es necesario
