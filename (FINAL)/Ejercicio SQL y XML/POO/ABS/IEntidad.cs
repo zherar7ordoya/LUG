8bd456dd7369b8c19b6225cfa8a47605ac150bcf
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-03-06 00:06:36 -0300

    2024-03-06

--- (FINAL)/Ejercicio SQL y XML/POO/ABS/IEntidad.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ABS
-{
-    // ...y éste el motivo por el cual en la base de datos uso "Codigo" como
-    // nombre de campo para la clave primaria (y no "Id").
-    public interface IEntidad
-    {
-        int Codigo { get; set; }
-    }
-}
